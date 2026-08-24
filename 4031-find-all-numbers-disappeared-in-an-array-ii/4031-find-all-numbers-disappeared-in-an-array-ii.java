ac49334f893837024c920f6694e3c9b63c9f5952
Ravi Maurya <79792076+ravimaurya333@users.noreply.github.com>
2026-08-24 09:17:02 +0530

    Time: 51 ms (0%), Space: 170.7 MB (0%) - LeetHub

--- 4031-find-all-numbers-disappeared-in-an-array-ii/4031-find-all-numbers-disappeared-in-an-array-ii.java
+++ 4031-find-all-numbers-disappeared-in-an-array-ii/4031-find-all-numbers-disappeared-in-an-array-ii.java
@@ -1,5 +1,33 @@
 class Solution {
     public List<List<Integer>> findDisappearedNumbers(int[] nums, int lower, int upper) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        List<List<Integer>> ls=new ArrayList<>();
         
+        int lwr=lower;
+        for(int i=0; i<n; i++){
+            List<Integer> temp=new ArrayList<>();
+            //missing found
+            if(nums[i]<lwr) continue;
+            if(nums[i]-lwr>0 && nums[i]<=upper){
+                temp.add(lwr);
+                temp.add(nums[i]-1);
+                ls.add(temp);
+                lwr=nums[i]+1;
+            }
+            
+            //same number
+            else if(nums[i]-lwr==0){
+                lwr=nums[i]+1;
+            }
+            if(lwr>upper) break;
+        }
+        if(lwr<=upper){
+            List<Integer> temp=new ArrayList<>();
+            temp.add(lwr);
+            temp.add(upper);
+            ls.add(temp);
+        }
+        return ls;
     }
 }
